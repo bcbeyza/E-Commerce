973e8ad824abc5ad51c64028a703bd651b30ccdd
sedanureren <sedanur.eren.25@gmail.com>
2023-09-13 12:00:48 +0300

    Product entity si için deneme yapıldı

---
+++ Business/Concrete/ProductManager.cs
@@ -0,0 +1,24 @@
+﻿using ECommerce.Business.Abstract;
+using ECommerce.DataAccess.Abstract;
+using ECommerce.Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ECommerce.Business.Concrete
+{
+    public class ProductManager : IProductService
+    {
+        IProductDal _productDal;
+        public ProductManager(IProductDal productDal)
+        {
+            _productDal = productDal;
+        }
+        public List<Product> GetAllByCategoryId(int id)
+        {
+            return _productDal.GetAll(p => p.CategoryId == id);
+        }
+    }
+}
--- ECommerce.Common/Entities/IEntity.cs
+++ ECommerce.Common/Entities/IEntity.cs
@@ -4,9 +4,9 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ECommerce.Entities
+namespace ECommerce.Common.Entities
 {
-    internal class Class1
+    public interface IEntity
     {
     }
 }
